77ba7b0866b4bc48ed5384d5649c5cbcd3dd8eac
Omar <omaraakamal@gmail.com>
2024-03-30 17:50:24 +0400

    updated code

--- context/src/pages/Homepage.jsx
+++ context/src/pages/Homepage.jsx
@@ -1,11 +1,16 @@
 const profileImg = 'https://i.imgur.com/i1gsj0v.png';
  
+import { useContext } from "react";
+
+import { ThemeContext } from "../context/theme.context";
 function HomePage() {
 
+  const diogoObject = useContext(ThemeContext)
   // useContext() hook is used to consume a hook
 
   return (
     <div className="HomePage">
+      {diogoObject.name}
       <h1>Hi! My Name is ...</h1>
       <img src={profileImg} className="profile" alt="profile" />
     </div>
