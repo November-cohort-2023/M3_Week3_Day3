bc3757e0df70e04cd7f0598c12f4b6e455b6b501
Omar <omaraakamal@gmail.com>
2024-03-30 13:36:53 +0400

    updated code

---
+++ project-management/routes/auth.routes.js
@@ -0,0 +1,30 @@
+ 
+const express = require("express");
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+ 
+const router = express.Router();
+const saltRounds = 10;
+
+
+// POST  /auth/signup
+// ...
+ 
+
+
+
+ 
+// POST  /auth/login
+// ...
+ 
+ 
+// GET  /auth/verify
+// ...
+
+
+
+
+
+
+module.exports = router;
